8102dcd3955ab91bd150a7cb0354057e49be6491
zubbyzillion <princewillbeluonwuogbo1@gmail.com>
2023-01-29 07:39:58 +0100

    Server's Okay

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-type': 'text/html'});
+    res.end('Servers Response');
+});
 
+server.listen(1337, '127.0.0.1', () => {
+    console.log('Server listening for requests');
 });
